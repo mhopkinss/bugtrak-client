748d389ad0f8ace80eb03ee0de797cd17a4bf8f1
Marcos Hopkins <marcos123hopkins@gmail.com>
2022-11-30 17:49:09 -0800

    fixed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NotFound from './pages/NotFound';
 import Project from './pages/Project';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: '/graphql',
   cache: new InMemoryCache(),
 })
 function App() {
